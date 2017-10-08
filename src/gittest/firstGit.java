73e54ad6cc7320672da84b8c85920f4d10444a1b
ChenQiangXiao <1056837317@qq.com>
2017-10-08 17:03:44 +0800

    第二次初始化提交

--- src/gittest/firstGit.java
+++ src/gittest/firstGit.java
@@ -3,5 +3,7 @@ package gittest;
 public class firstGit {
    public static void main(String[] args) {
 	System.out.println("hello");
+	System.out.println("fsdgdsf"); 
+	System.out.println("sys");
 }
 }
--- src/gittest/secondGit.java
+++ src/gittest/secondGit.java
@@ -3,5 +3,6 @@ package gittest;
 public class secondGit {
    public static void main(String[] args) {
 	System.out.println("hello2");
+	System.out.println("fsedg");
 }
 }
